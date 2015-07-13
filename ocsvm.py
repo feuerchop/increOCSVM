bug in gradient calculation removed

ocsvm.py
@@ -27,7 +27,6 @@ class OCSVM(object):
         self._data = data.Data()
 
     #returns trained SVM predictor given features (X)
-    #TODO: we need to store the key properties of model after training
     # Please check libsvm what they provide for output, e.g., I need to access to the sv_idx all the time
     def train(self, X):
         self._data.set_X(X)
@@ -60,7 +59,6 @@ class OCSVM(object):
         return pairwise_kernels(X, Y, "rbf", gamma=self._gamma)
 
     #compute Lagrangian multipliers
-    # TODO: I'd rather this part directly goes in train()
     def alpha(self, X):
         n_samples, n_features = X.shape
         K = self.gram(X)
@@ -95,7 +93,7 @@ class OCSVM(object):
     ### incremental
 
     def increment(self, x_c):
-
+        print self._data.get_alpha_s()
         epsilon = 0.001
          # calculate mu according to KKT-conditions
         #
@@ -105,17 +103,18 @@ class OCSVM(object):
         # calculate gradient of alpha_c
         alpha_c = 0
         grad_alpha_c = - self.gram(x_c) + self.gram(x_c, self._data.get_Xs()).dot(self._data.get_alpha_s()) + mu
-
-        #while grad_alpha_c[0] < 0 and alpha_c < self._data.get_C():
+        print "grad_alpha: " + str(self._data.get_alpha())
+        print "grad_alpha_s: " + str(self._data.get_alpha_s())
+        print "grad_alpha_c: " + str(grad_alpha_c)
+        #
         # just to test the loop
         len_s = len(self._data.get_alpha_s())
         Q = - inv(cat((cat(([[0]], np.ones((1,len_s))), axis=1),cat((np.ones((len_s,1)),self.gram(self._data.get_Xs())), axis=1
                         )),axis=0))
-        while True:
+
+        while grad_alpha_c[0] < 0 and alpha_c < self._data.get_C():
 
             # calculate beta
-            #TODO: optimize Q because inverse is computationally extensive
-
 
             beta = Q.dot(cat(([1], self.gram(x_c,self._data.get_Xs())[0]), axis=0))
 
@@ -163,14 +162,20 @@ class OCSVM(object):
 
 
             I_Rplus = np.all([gamma[1:] > epsilon, self._data.get_alpha_r() == self._data.get_C()],axis=0)
+            print "I_Rplus: "+ str(I_Rplus)
             I_Rplus_ind = [c for c,i in enumerate(I_Rplus) if i]
             I_Rminus = np.all([gamma[1:] < - epsilon, self._data.get_alpha_r() <= 1e-5],axis=0)
+            print "I_Rminus: " + str(I_Rminus)
             I_Rminus_ind = [c for c,i in enumerate(I_Rminus) if i]
 
             #calculate gradient of alpha (g_r)
             grad_alpha_r = - self.gram(self._data.get_Xr()).diagonal()  \
-              + self.gram(self._data.get_Xr()).dot(self._data.get_alpha_r()) \
+              + self.gram(self._data.get_Xr(),self._data.get_Xs()).dot(self._data.get_alpha_s()) \
               + mu * np.ones(len(self._data.get_alpha_r()))
+            #- self.gram(x_c) + self.gram(x_c, self._data.get_Xs()).dot(self._data.get_alpha_s()) + mu
+            print "alpha_r: " + str(self._data.get_alpha_r())
+            print "grad_alpha_r: " + str(grad_alpha_r)
+            print "gamma: " + str(gamma[1:])
             largest_increase_r = np.divide(- grad_alpha_r, gamma[1:])
             grad_alpha_I_Rplus = largest_increase_r[I_Rplus]
             grad_alpha_I_Rminus = largest_increase_r[I_Rminus]
@@ -178,7 +183,6 @@ class OCSVM(object):
             if len(cat((grad_alpha_I_Rplus, grad_alpha_I_Rminus))) > 0:
                 grad_alpha_c_R = cat((grad_alpha_I_Rplus, grad_alpha_I_Rminus)).min()
             else: grad_alpha_c_R = None
-
             #case 3: g_c becomes zero
             if gamma[0] > epsilon:
                 grad_alpha_c_g = np.divide(-grad_alpha_c, gamma[0])
@@ -196,21 +200,27 @@ class OCSVM(object):
             alpha_c += grad_alpha_c_max
             grad_alpha_c = gamma[0] * grad_alpha_c_max
             grad_alpha_r = gamma[1:] * grad_alpha_c_max
-            self._data.update_alpha_s(beta[1:]*grad_alpha_c_max)
+
+            print "old alpha_s: " + str(self._data.get_alpha_s())
+            print "new alpha_s: " + str(self._data.get_alpha_s() + beta[1:]*grad_alpha_c_S)
+
+            return 0
             if grad_alpha_c_max == grad_alpha_c_g:
+                print "break"
                 break
+            '''
             else:
                 if grad_alpha_c_max == grad_alpha_c_R:
                     #TODO: how to calculate alpha_k => (minimum point which will be a support vector then)
+                    # increment Q
+                    Q = cat((cat((Q, [np.zeros(Q.shape[1])]), axis=0), np.zeros((Q.shape[0] + 1, 1))), axis=1) + 1/gamma[0] * cat((beta,[1]), axis=1).dot(cat((beta,[1]), axis=1))
+                    # decrement Q
             # update Q
-            Q = cat((cat((Q, [np.zeros(Q.shape[1])]), axis=0), np.zeros((Q.shape[0] + 1, 1))), axis=1) + 1/gamma[0] * cat((beta,[1]), axis=1).dot(cat((beta,[1]), axis=1))
+
 
         print grad_alpha_c_max
         if grad_alpha_c <= 1e-5:
             self._data.add(x_c, alpha_c)
 
 
-
-
-
-
+'''

ocsvm_test.py
@@ -68,16 +68,18 @@ def standardExample():
 
 def incrementExample():
     # Generate train data
-    X = 0.3 * np.random.randn(10, 2)
+    X = 0.3 * np.random.randn(5, 2)
     X_train = np.r_[X + 2, X-2]
+    X_train = np.array([[ 2.05014156, 1.57874676], [ 1.77364132, 1.79817027], [ 2.168239, 1.79832986], [ 1.75084617, 1.97693673], [ 2.22726199, 2.54527565],
+            [-1.94985844, -2.42125324], [-2.22635868, -2.20182973], [-1.831761, -2.20167014], [-2.24915383, -2.02306327], [-1.77273801, -1.45472435]])
 
 
     # Generate some regular novel observations
-    X = 0.3 * np.random.randn(5, 2)
+    X = 0.3 * np.random.randn(1, 2)
     X_test = np.r_[X + 2,X-2]
 
     # Generate some abnormal novel observations
-    X_outliers = np.random.uniform(low=-4, high=4, size=(5, 2))
+    X_outliers = np.random.uniform(low=-4, high=4, size=(2, 2))
 
     clf1 = ocsvm.OCSVM("rbf", nu=0.5, gamma=3.1625)
     clf1.train(X_train)
@@ -88,13 +90,14 @@ def incrementExample():
     clf.train(X_train[:-1])
 
     #Plot the data
-    #plt.figure()
-    #plot(clf, X_train[:-1], X_test, X_outliers, 100, False)
-    print "point to increment"
+    plt.figure()
+    #plot(clf1, X_train[:-1], X_test, X_outliers, 100, False)
+    #plt.show()
+    #print "point to increment"
     clf.increment(X_train[-1:])
     #plt.figure()
     #plot(clf, X_train, X_test, X_outliers, 100, True)
-    #plt.show()
+
     #plt.savefig('test.pdf')
 
 if __name__ == "__main__":