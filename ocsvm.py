df4f5bd23988617c3ba1955bf4379eadfc1a24ef
dieulinhtran <mail@linhtran.de>
2015-07-16 21:31:20 +0200

    incremental alg

--- ocsvm.py
+++ ocsvm.py
@@ -122,22 +122,25 @@ class OCSVM(object):
         Kcc = 1
 
         # calculate mu according to KKT-conditions
-
         mu = 1 - self.gram(X[inds][0], X[inds]).dot(a[inds])
 
         # calculate gradient
         gc = - Kcc + Kcs.dot(a[inds]) + mu
         g = ones(l)
         g[inds] = zeros(ls)
-        g[indr] = ones((lr,1)) * mu - Krr.diagonal() + Krs.dot(a[inds])
+        g[indr] = - Krr.diagonal() + Krs.dot(a[inds]) + ones((lr,1)) * mu
 
         # initial calculation for beta
 
         Q = inv(vstack([hstack([0,ones(ls)]),hstack([ones((ls,1)), Kss])]))
 
         n = hstack([1, Kcs])
-
+        init_loop = True
         while gc < 0 and ac < C:
+            if init_loop:
+                init_loop = False
+            else:
+                init_loop = False
 
             # calculate beta
             beta = - Q.dot(n)
@@ -161,18 +164,23 @@ class OCSVM(object):
                 gsmax[IS_plus] = ones(len(betas[IS_plus]))*C-a[inds][IS_plus]
                 gsmax[IS_minus] = - a[inds][IS_minus]
 
-                absmin = absolute(gsmax).min()
-                gsmin = absmin * cmp(gsmax[where(absolute(gsmax) == absmin)],0)
-                ismin = where(gsmax == gsmin)
+                gsmax = divide(gsmax, betas)
+                gsmin = absolute(gsmax).min()
+                ismin = where(absolute(gsmax) == gsmin)
             else: gsmin = inf
             print "gsmin: " + str(gsmin)
             #case 2: Some g_i in R reaches zero
             if le > 0:
+                print "gammar: "+ str(gammar)
+                print "g[indr]: " +str(g[indr])
+                print "a[indr]: " + str(a[indr])
+                print C
                 Ie_plus = gammar[inde] > e
                 Ie_inf = gammar[inde] <= e
                 gec = zeros(le)
                 gec[Ie_plus] = divide(-g[indr][inde][Ie_plus], gammar[inde][Ie_plus])
                 gec[Ie_inf] = inf
+                print gec
                 gemin = gec.min()
                 iemin = where(gec == gemin)
             else: gemin = inf
@@ -185,9 +193,7 @@ class OCSVM(object):
                 goc[Io_inf] = inf
                 gomin = goc.min()
                 iomin = where(goc == gomin)
-                print gammar[indo]
-                print g[indr][indo]
-                print gomin
+
             else: gomin = inf
             print "gomin: " + str(gomin)
 
@@ -195,59 +201,42 @@ class OCSVM(object):
             if gammac > e: gcmin = - gc/gammac
 
             else: gcmin = inf
-            print gcmin
+            print "gcmin: " + str(gcmin[0])
             # case 4
             gacmin = C - ac
-            print gacmin
-
-            print min([gsmin, gemin, gomin, gcmin, gacmin])
-            print qmin
-            return 0
-            imin = where([gsmin, gemin, gomin, gcmin, gacmin] == gmin)
-            print qmin
-            print imin
-            return 0
-            
-
-            if grad_alpha_c_max == grad_alpha_c_R:
-                ind = abs(grad_alpha_r) == min(abs(grad_alpha_r))
-                ind_R = I_R_ind[np.where(ind == True)[0]]
-                grad_r_new = grad_alpha_r[I_R_ind[np.where(ind == True)[0]]]
-                print grad_r_new
-                X_r = self._data.get_Xr()[I_R_ind[np.where(ind == True)[0]]]
-                new_alpha = (grad_r_new + self.gram(X_r) - mu -self.gram(X_r, self._data.get_Xs()).dot(self._data.alpha_s()))
-
-                print "move from R to S => increment Q"
-                R = -1 * Q
-                R = cat((cat((R,zeros((1,R.shape[1]))),axis=0),zeros((R.shape[0]+1,1))),axis=1) + (1/gamma[0]) * cat((beta, [1]),axis=0)* np.array([cat((beta, [1]),axis=0)]).T
-                Q = -1 * R
-
-            else:
-                print "move from S to R => decrement Q"
-                grad_alpha_c_S_ind = I_S_ind[np.where(alpha_beta == grad_alpha_c_S)[0]] + 1
-                R = -1 * Q
-                for i in range(R.shape[0]):
-                    for j in range(R.shape[1]):
-                        if i != grad_alpha_c_S_ind and j != grad_alpha_c_S_ind:
-                            R[i][j] = R[i][j] - R[i][grad_alpha_c_S_ind] * R[grad_alpha_c_S_ind][j] / R[grad_alpha_c_S_ind][grad_alpha_c_S_ind]
-                R = np.delete(R, grad_alpha_c_S_ind,0)
-                R = np.delete(R, grad_alpha_c_S_ind,1)
-                Q = -1 * R
-
-
-            # update alpha after Q update (is better)
-            ac += grad_alpha_c_max
-
-
-
-            self._data.update_alpha_s(self._data.alpha_s() + beta[1:]*grad_alpha_c_max)
-            if grad_alpha_c_max == grad_alpha_c_g:
-                print "grad_alpha_c_max from c"
+            print "gacmin: " + str(gacmin)
+            gmin = min([gsmin, gemin, gomin, gcmin, gacmin])
+            imin = where([gsmin, gemin, gomin, gcmin, gacmin] == gmin)[0][0]
+            ac = ac + gmin
+            a[inds] = a[inds] + betas*gmin
+            g[indr] = g[indr] + gammar * gmin
+            gc = gc + gammac * gmin
+            print a[inds]
+            if imin == 0: # min = gsmin => move k from s to r
+                print "move k from s to r"
+                #update indeces
+                inds = np.all([a > e, a < C - e], axis=0)       # support vectors indeces
+                indr = np.any([a <= e, a >= C - e], axis=0)     # error and non-support vectors indeces
+                #update length of sets
+                ls = len(a[inds])                               # support vectors length
+                lr = len(a[indr])                               # error and non-support vectors length
+                #update kernel
+                Kss = self.gram(X[inds]) # kernel of support vectors
+                Krr = self.gram(X[indr]) # kernel of error vectors
+                Krs = self.gram(X[indr], X[inds]) # kernel of error vectors, support vectors
+                Kcs = self.gram(x_c, X[inds])[0]
+                Kcr = self.gram(x_c, X[indr])[0]
+                # update
+                mu = 1 - self.gram(X[inds][0], X[inds]).dot(a[inds])
                 break
-            loop_count += 1
+            elif imin == 1 or imin == 2: # min = gemin | gomin => move k from r to s
+                print "move k from s to r"
 
-        print grad_alpha_c_max
-        if gc <= 1e-5:
-            self._data.add(x_c, ac)
+                break
+            else: # k = c => terminate
+                print "k = c => terminate"
+                break
+            #TODO: Update Q
+
 
 
